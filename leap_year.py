943da91b55651f78c466d706df12bdc287c9ff27
Olav Vahtras <vahtras@kth.se>
2021-04-14 15:55:52 +0200

    introduce parametrized test

--- leap_year.py
+++ leap_year.py
@@ -1,20 +1,16 @@
-def test_leap_year():
-     assert is_leap_year(2000) == True
-     assert is_leap_year(1999) == False
-     assert is_leap_year(1998) == False
-     assert is_leap_year(1996) == True
-     assert is_leap_year(1900) == False
-     assert is_leap_year(1800) == False
-     assert is_leap_year(1600) == True
+import pytest
+
+@pytest.mark.parametrize(
+    'year,expected',
+    [(2000, True), (1999, False), (1998, False), (1996, True), (1900, False),
+     (1800, False), (1600, True)
+    ]
+)
+def test_leap_year(year, expected):
+     assert is_leap_year(year) == expected
 
 
 def is_leap_year(year):
     if year % 100 == 0:
-       if year % 400 == 0:
-          return True
-       else:
-          return False
-    if year % 4 == 0:
-        return True
-    else:
-        return False
+       return year % 400 == 0
+    return year % 4 == 0
